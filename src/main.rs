5701d91772f0133b3e515329601d07dc3dd8c886
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-24 21:26:26 -0500

    setup tracer

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
-use axum::{
-	routing::{get},
-	Router,
-};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use tracing::info;
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_level(true)
+        .init();
 
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
+    let app = Router::new().route("/", get(root));
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
-		println!("hi");
+    let port = 3000;
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    info!("Listening on port {}", port);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-// basic handler that responds with a static string
 async fn root() -> &'static str {
     "Hello, World!"
 }
