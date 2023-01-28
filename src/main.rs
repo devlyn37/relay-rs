720f748106ccd3868c7704c5d1443fb017a49e89
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-28 11:01:08 -0500

    get pk from .env loading in

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use axum::{
     Json, Router,
 };
 use dotenv::dotenv;
+use ethers_signers::{LocalWallet, Signer};
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
 use tracing::info;
 
 #[tokio::main]
@@ -23,7 +24,7 @@ async fn main() {
         .route("/", get(root))
         .route("/transaction", post(submit_transaction));
 
-    let port = 3000;
+    let port = std::env::var("PORT").map_or(3000, |s| s.parse().expect("Port should be a number"));
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     info!("Listening on port {}", port);
     axum::Server::bind(&addr)
@@ -37,9 +38,11 @@ async fn root() -> &'static str {
 }
 
 async fn submit_transaction(Json(payload): Json<SubmitTransaction>) -> impl IntoResponse {
-    let pk = std::env::var("PK").unwrap();
+    let pk_hex_string = std::env::var("PK").unwrap();
+    let wallet = LocalWallet::from_str(&pk_hex_string).unwrap();
 
-    info!("Transaction to submit: \n{:?}", payload);
+    info!("Wallet with address {}", wallet.address());
+
     let txn = Transaction {
         data: payload.data,
         id: 1,
