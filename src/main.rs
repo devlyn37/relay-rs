933a226fb7ac91202705bd8de371df4c6c78f26b
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-29 18:47:56 -0500

    Move provider to shared state, add nonce manager middleware

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use anyhow::{anyhow, Context, Error};
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
 use dotenv::dotenv;
-use ethers_core::types::{
-    serde_helpers::Numeric, Address, Eip1559TransactionRequest, TransactionReceipt,
+use ethers::{
+    core::types::{serde_helpers::Numeric, Address, Eip1559TransactionRequest},
+    middleware::{nonce_manager::NonceManagerMiddleware, signer::SignerMiddleware},
+    providers::{Http, Middleware, Provider},
+    signers::{LocalWallet, Signer},
 };
-use ethers_middleware::SignerMiddleware;
-use ethers_providers::{Http, Middleware, Provider};
-use ethers_signers::{LocalWallet, Signer};
 use serde::Deserialize;
-use std::{net::SocketAddr, str::FromStr};
+use std::{net::SocketAddr, str::FromStr, sync::Arc};
 use tracing::info;
+
+struct AppState {
+    provider: NonceManagerMiddleware<SignerMiddleware<Provider<Http>, LocalWallet>>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -26,11 +24,29 @@ async fn main() {
         .with_level(true)
         .init();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/transaction", post(submit_transaction));
-
+    let pk_hex_string =
+        std::env::var("PK").expect("Server not configured correctly, no private key");
+    let provider_url =
+        std::env::var("PROVIDER_URL").expect("Server not configured correctly, no provider url");
     let port = std::env::var("PORT").map_or(3000, |s| s.parse().expect("Port should be a number"));
+
+    let signer = LocalWallet::from_str(&pk_hex_string)
+        .expect("Server not configured correct, invalid private key");
+    let address = signer.address();
+
+    let provider = Provider::<Http>::try_from(provider_url)
+        .expect("Server not configured correctly, can't connect to provider");
+    let provider = SignerMiddleware::new_with_provider_chain(provider, signer)
+        .await
+        .expect("Could not initialize chain");
+    let provider = NonceManagerMiddleware::new(provider, address);
+
+    let shared_state = Arc::new(AppState { provider });
+
+    let app = Router::new()
+        .route("/transaction", post(relay_transaction))
+        .with_state(shared_state);
+
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
     info!("Listening on port {}", port);
     axum::Server::bind(&addr)
@@ -39,61 +55,26 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
-}
-
-async fn submit_transaction(Json(payload): Json<SubmitTransaction>) -> impl IntoResponse {
-    let pk_hex_string =
-        std::env::var("PK").expect("Server not configured correctly, no private key");
-    let provider_url =
-        std::env::var("PROVIDER_URL").expect("Server not configured correctly, no provider url");
-    let signer = LocalWallet::from_str(&pk_hex_string)
-        .expect("Server not configured correct, invalid private key");
-
-    let provider = Provider::<Http>::try_from(&provider_url)
-        .expect("Server not configured correctly, provider url or connection invalid");
-
+async fn relay_transaction(
+    State(state): State<Arc<AppState>>,
+    Json(payload): Json<SubmitTransaction>,
+) -> impl IntoResponse {
     let request = Eip1559TransactionRequest::new()
         .to(payload.to)
         .value(payload.value)
         .data(payload.data);
-
-    let client = SignerMiddleware::new_with_provider_chain(provider.clone(), signer.clone())
+    let pending_tx = state
+        .provider
+        .send_transaction(request, None)
         .await
-        .unwrap();
+        .expect("Something went wrong while submitting transaction");
+    let hash = pending_tx.tx_hash();
+    info!("Transaction sent, hash: {:?}.", hash);
 
-    let receipt = handle_transaction(request, client)
-        .await
-        .expect("Something went wrong when submitting transaction");
+    let receipt = pending_tx.confirmations(2).await.unwrap();
+    info!("{:?} mined.", hash);
 
     (StatusCode::OK, Json(receipt))
-}
-
-async fn handle_transaction(
-    txn_request: Eip1559TransactionRequest,
-    client: SignerMiddleware<Provider<Http>, LocalWallet>,
-) -> Result<TransactionReceipt, Error> {
-    info!(
-        "Wallet with address {}, is sending transaction {:?}",
-        client.signer().address(),
-        txn_request
-    );
-
-    let pending_tx = client
-        .send_transaction(txn_request, None)
-        .await
-        .with_context(|| "Error submitting transaction")?;
-
-    info!(
-        "Transaction sent, hash: {}.\nWaiting for it to be mined...",
-        pending_tx.tx_hash()
-    );
-
-    pending_tx
-        .confirmations(2)
-        .await?
-        .ok_or(anyhow!("Transaction was not mined"))
 }
 
 #[derive(Deserialize)]
