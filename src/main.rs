934f6dc8e08d22e61aa4fa969e4cd06e7b87f4c3
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-01-29 15:19:00 -0500

    Improve input validation, clean up

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ use axum::{
 };
 use dotenv::dotenv;
 use ethers_core::types::{
-    transaction::eip2718::TypedTransaction, Address, Eip1559TransactionRequest, TransactionReceipt,
-    U256,
+    serde_helpers::Numeric, Address, Eip1559TransactionRequest, TransactionReceipt,
 };
 use ethers_middleware::SignerMiddleware;
 use ethers_providers::{Http, Middleware, Provider};
@@ -45,17 +44,20 @@ async fn root() -> &'static str {
 }
 
 async fn submit_transaction(Json(payload): Json<SubmitTransaction>) -> impl IntoResponse {
-    let pk_hex_string = std::env::var("PK").unwrap();
-    let provider_url = std::env::var("PROVIDER_URL").unwrap();
-    let signer = LocalWallet::from_str(&pk_hex_string).unwrap();
-    let address: Address = payload.to.parse().unwrap();
-    let provider =
-        Provider::<Http>::try_from(&provider_url).expect("could not instantiate HTTP Provider");
+    let pk_hex_string =
+        std::env::var("PK").expect("Server not configured correctly, no private key");
+    let provider_url =
+        std::env::var("PROVIDER_URL").expect("Server not configured correctly, no provider url");
+    let signer = LocalWallet::from_str(&pk_hex_string)
+        .expect("Server not configured correct, invalid private key");
+
+    let provider = Provider::<Http>::try_from(&provider_url)
+        .expect("Server not configured correctly, provider url or connection invalid");
 
     let request = Eip1559TransactionRequest::new()
-        .to(address)
-        .value(U256::from_dec_str(&payload.value).unwrap())
-        .data(hex::decode(payload.data).unwrap());
+        .to(payload.to)
+        .value(payload.value)
+        .data(payload.data);
 
     let client = SignerMiddleware::new_with_provider_chain(provider.clone(), signer.clone())
         .await
@@ -79,7 +81,7 @@ async fn handle_transaction(
     );
 
     let pending_tx = client
-        .send_transaction(TypedTransaction::Eip1559(txn_request), None)
+        .send_transaction(txn_request, None)
         .await
         .with_context(|| "Error submitting transaction")?;
 
@@ -94,9 +96,10 @@ async fn handle_transaction(
         .ok_or(anyhow!("Transaction was not mined"))
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct SubmitTransaction {
-    to: String,
-    value: String,
-    data: String,
+    to: Address,
+    value: Numeric,
+    #[serde(with = "hex::serde")]
+    data: Vec<u8>,
 }
