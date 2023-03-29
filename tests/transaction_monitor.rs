f18c613668eca1879f91c5255c0667a8fce88b14
Devlyn Dorfer <devlyndorfer@gmail.com>
2023-03-29 08:00:33 -0400

    Finish resubmission test

--- tests/transaction_monitor.rs
+++ tests/transaction_monitor.rs
@@ -12,7 +12,7 @@ use sqlx::{MySql, Pool};
 use tokio::time::{sleep, Duration};
 
 #[sqlx::test]
-async fn chain_monitor_happy_path(pool: Pool<MySql>) {
+async fn transaction_monitor_resubmission(pool: Pool<MySql>) {
     tracing_subscriber::fmt()
         .compact()
         .with_file(true)
@@ -24,40 +24,28 @@ async fn chain_monitor_happy_path(pool: Pool<MySql>) {
     // Need to either set the gas values such the test txn doesn't mine
     // fallback to dropping it from the mempool
     let anvil = Anvil::new()
-        .args(vec![
-            "--no-mining",
-            "--gas-price",
-            "500",
-            "--base-fee",
-            "50",
-        ])
+        .args(vec!["--no-mining", "--base-fee", "50"])
         .spawn();
-    let chain_id = anvil.chain_id();
-
     let provider =
         Provider::<Http>::try_from(anvil.endpoint()).expect("Should be able to connect to anvil");
     let wallet: LocalWallet = anvil.keys().first().unwrap().clone().into();
-    let wallet = wallet.with_chain_id(chain_id);
+    let wallet = wallet.with_chain_id(anvil.chain_id());
 
     let recipient = anvil.addresses()[1];
 
-    let repo = DbTxRequestRepository::new(pool);
-    let mut monitor = TransactionMonitor::new(repo);
+    let mut monitor = TransactionMonitor::new(DbTxRequestRepository::new(pool));
     monitor
         .setup_monitor(wallet, provider.clone(), Chain::AnvilHardhat, 1)
         .await
         .unwrap();
 
-    let request = Eip1559TransactionRequest::new()
-        .to(recipient)
-        .value(1)
-        .max_fee_per_gas(100)
-        .max_priority_fee_per_gas(1);
+    let request = Eip1559TransactionRequest::new().to(recipient).value(1);
     let id = monitor
         .send_monitored_transaction(request, Chain::AnvilHardhat)
         .await
         .unwrap();
 
+    // Send the first request
     let result = monitor
         .get_transaction_status(id)
         .await
@@ -67,26 +55,29 @@ async fn chain_monitor_happy_path(pool: Pool<MySql>) {
     assert!(!mined);
     println!("mined {}, hash {}", mined, hash);
 
-    println!("Mining a block");
-    let test = provider
+    // Drop the transaction so it doesn't get mined
+    provider
+        .request::<_, U256>("anvil_dropTransaction", vec![format!("{:?}", hash)])
+        .await
+        .expect("dropping transaction should work");
+
+    // Mine a block so that the monitor resubmits the tx
+    provider
         .request::<_, U256>("evm_mine", None::<()>)
         .await
         .expect("mining should work");
 
-    println!("Sleeping while waiting for some blocks to mine...");
-    sleep(Duration::from_secs(15)).await; // let some blocks get mined
-    println!("Done sleeping!");
-    let receipt = provider
-        .get_transaction_receipt(hash)
-        .await
-        .expect("Grabbing the transaction hash should work");
-    println!("{:?}", receipt);
+    println!("Sleeping, waiting for the monitor to resubmit");
+    sleep(Duration::from_secs(15)).await;
 
-    let block_number = provider
-        .get_block_number()
+    println!("Mining another block");
+    provider
+        .request::<_, U256>("evm_mine", None::<()>)
         .await
-        .expect("Fetching the block number should work");
-    println!("Here's the latest block number {:?}", block_number);
+        .expect("mining should work");
+
+    println!("Sleeping, waiting for the monitor to process");
+    sleep(Duration::from_secs(15)).await; // let some blocks get mined
 
     let result = monitor
         .get_transaction_status(id)
@@ -95,6 +86,12 @@ async fn chain_monitor_happy_path(pool: Pool<MySql>) {
     assert!(result.is_some());
 
     let (mined, hash) = result.expect("The result should be some");
+    let receipt = provider
+        .get_transaction_receipt(hash)
+        .await
+        .expect("Grabbing the transaction hash should work");
+    println!("Here's the receipt to show the tx was mined\n{:?}", receipt);
+
     println!("mined {}, hash {}", mined, hash);
     assert!(mined);
 }
